9cea908c79f7f1333cf638dbf4b50293fb217d5a
NachoCanto <ignacio.canto.ceb@immune.institute>
2021-11-23 13:39:33 +0100

    challenge

---
+++ challenge1_NachoCanto.py
@@ -0,0 +1,4 @@
+print("Bienvenido al programa contador de letras")
+nombre = input("\n\n¿Cual es tu nombre?")
+print(f'\nHola {nombre.title}. \nvamos a contar el numero de letras de tu mensaje')
+letra = input("\n\nQue letra quieres contar")
